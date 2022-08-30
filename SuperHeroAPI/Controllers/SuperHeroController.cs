4339769ea13b80d4b40821db832aff8c251df407
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-08-30 09:19:03 +0600

    project setup with get, post and put

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heros = new List<SuperHero>
+        {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Shahdat",
+                    FirstName = "Sajib",
+                    LastName = "Shahdat",
+                    Place = "Bangladesh"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name = "Arifa",
+                    FirstName = "Arifa",
+                    LastName = "Sultana",
+                    Place = "Rajshahi"
+                }
+        };
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heros);
+        }
+
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<SuperHero>> Get(int Id)
+        {
+            var hero = heros.Find(h => h.Id == Id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+            return Ok(hero);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heros.Add(hero);
+            return Ok(heros);
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatehero)
+        {
+            var hero = heros.Find(h => h.Id == updatehero.Id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+            
+            hero.Name = updatehero.Name;
+            hero.FirstName = updatehero.FirstName;  
+            hero.LastName = updatehero.LastName;    
+            hero.Place = updatehero.Place; 
+
+            return Ok(heros);
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public String Name { get; set; } = String.Empty;
+        public String FirstName { get; set; } = String.Empty;
+        public String LastName { get; set; } = String.Empty;
+        public String Place { get; set; } = String.Empty;
+    }
+}
